2feafc61f8408835517e4126e60699dd49570d2f
Patrick Taylor <hello@patricktaylor.dev>
2024-06-01 15:06:42 +0100

    Bump css-loader version 🏄🏻‍♂️

--- tests/loader.spec.js
+++ tests/loader.spec.js
@@ -2,9 +2,9 @@ import loader from '../src/loader'
 
 import compiler from './compiler'
 
-describe('loader', () => {
+const exampleCSS = 'h1 {\n  color: #FF69B4;\n}\n'
 
-  const exampleCSS = 'h1 {\\n  color: #FF69B4;\\n}\\n'
+describe('loader', () => {
 
   let mockContext
 
@@ -23,40 +23,48 @@ describe('loader', () => {
 
   it('encapsulates CSS in the specified class', () => {
     mockContext.getOptions.mockReturnValue({ className: 'test' })
+
     const result = loader.call(mockContext, exampleCSS)
     expect(result).toBe(`.test { ${exampleCSS} }`)
   })
 
   it('throws an error if className is not valid', () => {
     mockContext.getOptions.mockReturnValue({ className: 1 })
+
     expect(() => loader.call(mockContext, exampleCSS)).toThrow('Invalid options object. Encapsulated CSS Loader has been initialized using an options object that does not match the API schema.\n - options.className should be a string.\n   -> Define the class name in which you would like to encapsulate your css.')
   })
 
   it('throws an error if className is not provided', () => {
     mockContext.getOptions.mockReturnValue({})
+
     expect(() => loader.call(mockContext, exampleCSS)).toThrow('className must be provided and must be a string')
   })
 
-  describe('with Webpack', () => {
+})
 
-    it('encapsulates a css file', async () => {
-      const stats = await compiler('example.css', { className: 'test' })
-      const output = stats.toJson({ source: true }).modules[0].source
+describe('loader with Webpack', () => {
 
-      expect(output).toContain(`___CSS_LOADER_EXPORT___.push([module.id, ".test { ${exampleCSS} }", ""]);`)
-    })
+  it('encapsulates a css file', async () => {
+    const stats = await compiler('example.css', { className: 'test' })
+    const output = stats.toJson({ source: true }).modules[0].source
 
-    it('encapsulates an scss file', async () => {
-      const stats = await compiler('example.scss', { className: 'test' })
-      const output = stats.toJson({ source: true }).modules[0].source
+    expect(output).toContain(`___CSS_LOADER_EXPORT___.push([module.id, \`.test { ${exampleCSS} }\`, ""]);`)
+  })
 
-      expect(output).toContain(`___CSS_LOADER_EXPORT___.push([module.id, ".test { ${exampleCSS} }", ""]);`)
-    })
+  it('encapsulates an scss file', async () => {
+    const stats = await compiler('example.scss', { className: 'test' })
+    const output = stats.toJson({ source: true }).modules[0].source
 
-    it('throws an error if className is not provided', async () => {
-      expect(async () => await compiler('example.scss', {})).toThrow('className must be provided and must be a string')
-    })
+    expect(output).toContain(`___CSS_LOADER_EXPORT___.push([module.id, \`.test { ${exampleCSS} }\`, ""]);`)
+  })
 
+  it('throws an error if className is not provided', async () => {
+    expect.assertions(1)
+    try {
+      await compiler('example.scss', {})
+    } catch (error) {
+      expect(error[0].message).toContain('Error: className must be provided and must be a string')
+    }
   })
 
 })
